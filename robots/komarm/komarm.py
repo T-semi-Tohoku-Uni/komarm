9a3ab34997a0ca5c24e1014bf6b0bca23645fd0d
shibatakeigo <sagankeigo17@gmail.com>
2026-05-26 03:11:18 +0900

    feat(submodule): fix base set to false

--- robots/komarm/komarm.py
+++ robots/komarm/komarm.py
@@ -9,7 +9,7 @@ TEMPLATE_ASSETS_DATA_DIR = Path(__file__).resolve().parent
 
 KOMARM_CFG = ArticulationCfg(
     spawn=sim_utils.UrdfFileCfg(
-        fix_base=True,
+        fix_base=False,
         replace_cylinders_with_capsules=True,
         asset_path=f"{TEMPLATE_ASSETS_DATA_DIR}/inrof2026_koma_urdf/urdf/komarm.urdf",
         activate_contact_sensors=False,
