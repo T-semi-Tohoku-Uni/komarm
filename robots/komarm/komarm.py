ebd332fd29848f66a051d5ec4488068bef162f4c
shibatakeigo <sagankeigo17@gmail.com>
2026-05-28 16:35:14 +0900

    feat(pos): change initial position

--- robots/komarm/komarm.py
+++ robots/komarm/komarm.py
@@ -33,8 +33,8 @@ KOMARM_CFG = ArticulationCfg(
         pos=(0.0, 0.0, 0.02),
         joint_pos={
             "Revolute_1": 0.0,
-            "Revolute_2": 0.0,
-            "Revolute_3": 0.0,
+            "Revolute_2": -1.3,
+            "Revolute_3": 1.0,
             "Revolute_4": 0.0,
             "Revolute_5": 0.0,
             "Revolute_6": 0.0,
