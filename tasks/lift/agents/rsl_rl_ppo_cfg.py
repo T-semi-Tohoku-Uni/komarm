a5e0d1049baedbdda6163cf254f5a275dea2e8d8
shibatakeigo <sagankeigo17@gmail.com>
2026-05-29 11:00:18 +0900

    feat(wandb): add wandb log

--- tasks/lift/agents/rsl_rl_ppo_cfg.py
+++ tasks/lift/agents/rsl_rl_ppo_cfg.py
@@ -21,6 +21,11 @@ class LiftCubePPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
     max_iterations = 1500
     save_interval = 50
+
+    # record log
+    logger = "wandb"
+    wandb_project = "komarm-lift"
+
     experiment_name = "lift"
     empirical_normalization = False
     policy = RslRlPpoActorCriticCfg(
